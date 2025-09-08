13f91918cb13d3cc6570a94cc510b8046f1349a9
bahelmanoj <bahel.manoj74@gmail.com>
2025-09-08 21:11:25 +0530

    commit

---
+++ m3t2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  8 20:57:07 2025
+
+@author: Administrator
+"""
+
+s=0
+for i in range(50):
+    s=s+i
+print('The sum of numbers from 1 to 50 is :',s)
